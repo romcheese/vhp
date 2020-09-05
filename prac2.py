9b4d26ed67e8a65a0944e9953cc44bda7809b885
romcheese <65137270+romcheese@users.noreply.github.com>
2020-09-05 11:54:34 +0300

    Delete prac2.py

--- prac2.py
+++
@@ -1,8 +0,0 @@
-def odometer(array):
-    distance = 0
-    for i in range(len(array)):
-        if i == 0 or i % 2 == 0:
-            distance += array[i]
-        else:
-            continue
-    return distance
