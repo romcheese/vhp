9bcc77b4d7bc0ba6074879b81e3c9dfa6564ba47
romcheese <65137270+romcheese@users.noreply.github.com>
2020-09-30 10:48:42 +0300

    Add files via upload

---
+++ prac9.py
@@ -0,0 +1,73 @@
+s = 'отдай мою кроличью лапку...'
+# s = 'омоюу толл дюиа акчп йрьк...'
+# https://skillsmart.ru/algo/lvl1/ab53.html
+
+def TheRabbitsFoot(s, encode: bool):
+    if encode == True:
+        for i in s:
+            if i.isalpha() == False:
+                s = s.replace(i, '')
+
+        l = len(s)**0.5
+        lb = len(s) // l # lower border
+        ub = lb + 1 # upper border
+        l2 = len(s) # длина слов без пробелов
+
+        while True: # создание матрицы
+            result = lb * ub
+            if result < len(s):
+                lb += 1
+            else:
+                break
+
+            phrase = []
+            count = 0
+            while True:
+                slice = s[:int(ub)]
+                count = count + len(slice)
+                phrase.append(slice)
+                if count >= l2:
+                    break
+                else:
+                    s = s[int(ub):]
+            st = ''
+
+
+            for i in range(int(lb)): # encode
+                for word in phrase:
+                    try:
+                        x = word[i]
+                        st += str(x)
+                    except IndexError:
+                        break
+                st += ' ' # correct before here
+            return st
+    elif encode == False:
+        for i in s:
+            if i.isalpha() == False:
+                s = s.replace(i, '')
+        # biggest i
+        biggest_i = 0
+        decode = ''
+
+        for word in s:
+            i = len(word)
+            if i > biggest_i:
+                biggest_i = i
+
+
+        for i in range(biggest_i):
+            for word in s:
+                try:
+                    decode += word[i]
+                except IndexError:
+                    return decode
+
+#
+#
+#
+#
+#
+# #
+x = TheRabbitsFoot(s, True)
+print(x)
