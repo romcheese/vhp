2a948d88aee420c7a1a1919bccbc09178372e7d9
romcheese <65137270+romcheese@users.noreply.github.com>
2020-12-04 21:47:24 +0300

    Add files via upload

--- prac18.py
+++ prac18.py
@@ -9,21 +9,22 @@ def MisterRobot(*args):
     checked = []
     while True:
         if data != sorted(data):
-            if data not in checked:
-                for i in range(len(data)):
-                    c = data[i:i+3]
-                    if sorted(c) != c:
-                        checked.append(data)
-                        for k in range(3):
-                            ch = [c[k], c[k-2], c[k-1]]
-                            if ch == sorted(c):
-                                data[i:i+3] = ch
-                                break
-            else:
+            for i in range(len(data)):
+                c = data[i:i+3]
+                if sorted(c) != c:
+                    for k in range(3):
+                        ch = [c[k], c[k-2], c[k-1]]
+                        if ch == sorted(c):
+                            data[i:i+3] = ch
+            if data in checked and data != sorted(data):
                 return False
+            checked.append(data)
+
+
+
         else:
             return True
 
-# 
-# x = MisterRobot(7, [1,3,4,5,2,6,7])
+# #
+# x = MisterRobot(7, 1,3,4,5,6,2,7)
 # print(x)
