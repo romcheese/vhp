b3442c4358e80979e888744e6935daed8a4ff979
romcheese <65137270+romcheese@users.noreply.github.com>
2021-02-11 17:01:51 +0300

    Add files via upload

--- prac21.py
+++ prac21.py
@@ -1,2 +1,30 @@
-def BiggerGreater(str:str):
-    return 'prolonger'
+# https://skillsmart.ru/algo/lvl1/f27e.html
+from itertools import permutations
+
+def BiggerGreater(s:str):
+    word = []
+    for letter in s:
+        word.append(letter)
+
+    vars = []
+
+    for i in permutations(s):
+        if list(i) > word:
+            vars.append(i)
+
+    vars.sort()
+    final_word = ''
+
+    try:
+        for el in vars[0]:
+            final_word += str(el)
+    except IndexError:
+        return ''
+
+    return final_word
+
+#
+#
+#
+# x = BiggerGreater('fff')
+# print(x)
